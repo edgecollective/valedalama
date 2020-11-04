5abdbb3c5e10602f84eb7cf27eaeb1a68f0b717e
Don Blair <donblair@gmail.com>
2020-11-04 12:27:41 -0500

    update

---
+++ greenhouse/gateway/heltec_wifi_lora_bayou_farmos/sample_credentials.h
@@ -0,0 +1,5 @@
+const char *SSID = "(YOUR WIFI SSID)";
+const char *WiFiPassword = "(YOUR WIFI PASSWORD)";
+const char* bayou_url = "(YOUR BAYOU URL)";
+const char* bayou_privkey = "(YOUR BAYOU PRIVATE KEY)";
+const char* farmOS_url = "(YOUR FARMOS URL)";
